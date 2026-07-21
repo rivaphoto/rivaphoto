17eb61ff424cf312835c9cfdf47af3c0ddd6ac5c
Alexa Rivadeneira <alexariva@gmail.com>
2026-07-21 12:26:32 -0400

    Rebuild site with data-driven photo galleries

---
+++ gallery-data.js
@@ -0,0 +1,59 @@
+// To add a photo: drop the image file into /images, then add its filename
+// to the matching array below. To add a whole new section, copy one of the
+// blocks and give it a new key, title, and (if it should live in a nav
+// dropdown) add a link for it in index.html's <nav>.
+
+const GALLERIES = {
+  portraits: {
+    title: "Portraits",
+    images: [
+      "portraits-01.jpg",
+      "portraits-02.jpg",
+      "portraits-03.jpg",
+      "portraits-04.jpg",
+      "portraits-05.jpg",
+      "portraits-06.jpg",
+    ],
+  },
+  travel: {
+    title: "Travel",
+    images: [
+      "travel-01.jpg",
+      "travel-02.jpg",
+      "travel-03.jpg",
+      "travel-05.jpg",
+      "travel-06.jpg",
+      "travel-07.jpg",
+      "travel-08.jpg",
+      "travel-09.jpg",
+      "travel-11.jpg",
+      "travel-12.jpg",
+      "travel-13.jpg",
+      "travel-14.jpg",
+      "travel-15.jpg",
+      "travel-17.jpg",
+      "travel-18.jpg",
+    ],
+  },
+  street: {
+    title: "Street Photography",
+    images: ["street-01.jpg", "street-02.jpg"],
+  },
+  views: {
+    title: "Views",
+    images: ["views-01.jpg"],
+  },
+  digital: {
+    title: "Street",
+    subtitle: "Shooting on a FUJIFILM X100VI",
+    images: [
+      "digital-street-01.jpg",
+      "digital-street-02.jpg",
+      "digital-street-03.jpg",
+      "digital-street-04.jpg",
+      "digital-street-05.jpg",
+      "digital-street-06.jpg",
+      "digital-street-07.jpg",
+    ],
+  },
+};
